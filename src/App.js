1acfc5b9518802fcebe6045e79fa3ec17b1f3c0c
priya-tiwari <tiwaripriya5062@gmail.com>
2020-07-15 01:45:39 +0530

    Added setState feature and table data Uploading

--- src/App.js
+++ src/App.js
@@ -1,83 +1,103 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button,Container,Row,Col,Table} from 'reactstrap';
 
-const state = React.createContext({
-  "pending":["1"],
-  "ongoing":[],
-  "completed":[]
-});
-function List(props){
-    if(state._currentValue[props.data].length === 0)
-      return <tr><td>Empty List :(</td></tr>
-    else{
-        var wholelist =  state._currentValue[props.data].map(function(a){
-            return <tr><td>{a}</td></tr>
-        });
-        return wholelist;
-    }
-}
-function ListCards(props){
-    // var list = GetList(props.listname);
-    return (
-      <Col sm="4">
-        <Table id = {props.listname}>
-          <thead>
-            <tr>
-              <th if={props.listname === "pending"}>PENDING TASKS</th>
-            </tr>
-          </thead>
-          <tbody>
-              <List data={props.listname}/>
+  function Tile(props){
+      console.log(props)
+      return (<tr key={'key'+props.a}>
+        <td>{props.index}</td>
+      </tr>)
+  }
+  function List(props){
+    if(props.data[props.listname].length === 0)
+        return <tr><td>Empty List :(</td></tr>
+    // else return <tr><td> List :(</td></tr>
+      else{
+          var name = props.data[props.listname];
+          console.log(name);
+          return name.map((index,a) => (
+              <Tile index={index} a={a}/>
+          ));
+      }
+  }
+  function ListCards(props){
+      // console.log(props);
+      return (
+        <Col sm="4">
+          <Table>
+            <thead>
+              <tr>
+                {props.listname === "pending".toString() && <th>PENDING TASKS</th>}
+                {props.listname === "ongoing".toString() && <th>ONGOING TASKS</th>}
+                {props.listname === "completed".toString() && <th>COMPLETED TASKS</th>}
+              </tr>
+            </thead>
+            <tbody id={props.listname}>
+                <List data={props.data} listname={props.listname}/>
             </tbody>
-        </Table>
-      </Col>
-    )
-}
-function AddTo(form){
-    var val = document.getElementById("workname").value;
-    state._currentValue[form.moveto].push({"name":val});
-    document.getElementById("pending").value = <ListCards listname={"pending"}/>
-}
-function Input(props){
-    switch(props.type){
-       case "text": return <input type="text" name={props.name} id={props.name}/>
-       case "textarea": return <div><textarea name={props.name}/></div>
+          </Table>
+        </Col>
+      )
+  }
+  
+  function Input(props){
+      switch(props.type){
+        case "text": return <input type="text" name={props.name} id={props.name}/>
+        case "textarea": return <div><textarea name={props.name}/></div>
+      }
+  }
+
+class App extends Component{
+    constructor(props){
+      super();
+      this.state = {
+        "pending":[],
+        "ongoing":[],
+        "completed":[]
+      };
+      // this.state = this.state.bind(this);
     }
-}
-function ButtonFun(form){
-  return <Button color="success" name={form.name} onClick={()=>AddTo(form)}>Add</Button>
-}
-function App() {  
-  return (
-    <div className="App ">
-      <Container>
-        <Row>
-          <Col>
-            <h1>My ToDo List</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm="2">
-          </Col>
-          <Col sm="8">
-              <span><Input type={"text"} name={"workname"}/></span>
-              <span> </span>
-              <span><ButtonFun type={"button"} name={"submit"} moveto={"pending"} /></span>
-          </Col>
-          <Col sm="2">
-          </Col>
-        </Row><br/>
-        <Row>
-            <ListCards listname={"pending"}/>
-            <ListCards listname={"ongoing"}/>
-            <ListCards listname={"completed"}/>
-        </Row>
-      </Container>
-    </div>
-  );
+    
+    AddTo = ()=>{
+        var val = document.getElementById("workname").value;
+        document.getElementById("workname").value = "";
+        var x = this.state;
+        x["pending"].push(val);
+        this.setState(x);
+        console.log(this.state);
+    }
+
+    render(){
+      return(
+      <div className="App ">
+        <Container>
+          <Row>
+            <Col>
+              <h1>My ToDo List</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="2">
+            </Col>
+            <Col sm="8">
+                <span><Input type={"text"} name={"workname"}/></span>
+                <span> </span>
+                <span><Button color="success" name="add" onClick={this.AddTo}>Add</Button></span>
+            </Col>
+            <Col sm="2">
+            </Col>
+          </Row><br/>
+          <Row>
+              <ListCards listname="pending" data={this.state}/>
+              <ListCards listname="ongoing" data={this.state}/>
+              <ListCards listname="completed" data={this.state}/>
+          </Row>
+        </Container>
+      </div>
+      )
+  }
 }
 
 export default App;
